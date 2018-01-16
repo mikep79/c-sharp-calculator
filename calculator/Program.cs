6e3ecc93d411a71ddfae522f06a536ad35c79aa3
Mike Pettman <mp_palmatum@yahoo.com>
2018-01-16 16:27:56 -0600

    add and subtract 2 ints classes made

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace calculator
 {
+    public class AddInt
+    {
+        public int FirstNumber;
+        public int SecondNumber;
+        public void AddTwoNumbers()
+        {
+            Console.WriteLine(FirstNumber + " + " + SecondNumber + " = " + (FirstNumber + SecondNumber));
+        }
+    }
+    public class SubtractInt
+    {
+        public int FirstNumber;
+        public int SecondNumber;
+        public void SubtractTwoNumbers()
+        {
+            Console.WriteLine(FirstNumber + " - " + SecondNumber + " = " + (FirstNumber - SecondNumber));
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            AddInt trialRun = new AddInt();
+            trialRun.FirstNumber = 5;
+            trialRun.SecondNumber = 7;
+            trialRun.AddTwoNumbers();
+            var subtractTrial = new SubtractInt();
+            subtractTrial.FirstNumber = 5;
+            subtractTrial.SecondNumber = 7;
+            subtractTrial.SubtractTwoNumbers();
+
         }
     }
 }
